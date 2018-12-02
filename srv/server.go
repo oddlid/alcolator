cc581a3c7de29ffbe8f194e81b002e264464d7c2
Odd E. Ebbesen <oddebb@gmail.com>
2018-12-02 17:37:56 +0100

    First proper working version of server

---
+++ srv/server.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"strconv"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/oddlid/alcolator"
+	"github.com/urfave/cli"
+	log "github.com/Sirupsen/logrus"
+)
+
+var (
+	VERSION string = "0.0.1"
+	ctmpl          = template.Must(template.ParseFiles("apkform.html"))
+)
+
+type FormData struct {
+	HasData               bool
+	DrinkName             string
+	DrinkVolML            int
+	DrinkPCT              float64
+	DrinkPrice            float64
+	ResultWaterML         int
+	ResultAlcML           int
+	ResultPricePerAlcML   float64
+	ResultPricePerDrinkML float64
+}
+
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	hasData := r.FormValue("dsub") != ""
+
+	if !hasData {
+		ctmpl.Execute(w, nil)
+		return
+	}
+
+	fd := FormData{}
+	fd.HasData = hasData
+	if !fd.HasData {
+		log.Warnf("Should have form data, but it seems to be missing... From: %q", r.RemoteAddr)
+	}
+
+	fd.DrinkName = r.FormValue("dname")
+	var err error
+	fd.DrinkVolML, err = strconv.Atoi(r.FormValue("dvolml"))
+	if err != nil {
+		log.Errorf("Failed to convert dvolml. Input: %q from %q", r.FormValue("dvolml"), r.RemoteAddr)
+	}
+	fd.DrinkPCT, err = strconv.ParseFloat(r.FormValue("dpct"), 64)
+	if err != nil {
+		log.Errorf("Failed to convert dpct. Input: %q from %q", r.FormValue("dpct"), r.RemoteAddr)
+	}
+	fd.DrinkPrice, err = strconv.ParseFloat(r.FormValue("dprice"), 64)
+	if err != nil {
+		log.Errorf("Failed to convert dprice. Input: %q from %q", r.FormValue("dprice"), r.RemoteAddr)
+	}
+
+	d := &alcolator.Drink{
+		Name:     fd.DrinkName,
+		VolumeML: fd.DrinkVolML,
+		AlcPCT:   fd.DrinkPCT,
+		Price:    fd.DrinkPrice,
+	}
+
+	fd.ResultWaterML = d.WaterML()
+	fd.ResultAlcML = d.AlcML()
+	fd.ResultPricePerAlcML = d.PricePerAlcML()
+	fd.ResultPricePerDrinkML = d.PricePerDrinkML()
+
+	ctmpl.Execute(w, &fd)
+}
+
+func serve(ctx *cli.Context) error {
+	addr := ctx.String("listen")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", CalcHandler)
+	return http.ListenAndServe(addr, r)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "AlcoLatorSrv"
+	app.Usage = "Calculate drink values"
+	app.Copyright = "(c) 2018 Odd Eivind Ebbesen"
+
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Odd E. Ebbesen",
+			Email: "oddebb@gmail.com",
+		},
+	}
+	app.Version = VERSION
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "log-level",
+			Value: "info",
+			Usage: "Log `level` (options: debug, info, warn, error, fatal, panic)",
+		},
+		cli.BoolFlag{
+			Name:   "debug, d",
+			Usage:  "Run in debug mode",
+			EnvVar: "DEBUG",
+		},
+		cli.StringFlag{
+			Name:  "listen, l",
+			Usage: "`ADDR` to listen on",
+			Value: ":9600",
+		},
+	}
+
+	app.Before = func(c *cli.Context) error {
+		log.SetOutput(os.Stderr)
+		level, err := log.ParseLevel(c.String("log-level"))
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		log.SetLevel(level)
+		if !c.IsSet("log-level") && !c.IsSet("l") && c.Bool("debug") {
+			log.SetLevel(log.DebugLevel)
+		}
+		log.SetFormatter(&log.TextFormatter{
+			DisableTimestamp: false,
+			FullTimestamp:    true,
+		})
+		return nil
+	}
+
+	app.Action = serve
+	app.Run(os.Args)
+}
