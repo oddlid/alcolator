17bfafb3e37024af62533bdcf79b66d250e593cd
Odd E. Ebbesen <oddebb@gmail.com>
2019-11-29 22:02:17 +0100

    Changed import path for logrus

--- srv/server.go
+++ srv/server.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/gorilla/mux"
 	"github.com/oddlid/alcolator"
 	"github.com/oddlid/alcolator/srv/assets"
