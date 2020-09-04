c8c2d33cc419266d6c6aebb6aa69b0e2ceadda95
Odd E. Ebbesen <oddebb@gmail.com>
2020-09-05 00:21:40 +0200

    Changes from almost a year ago. Not important, but needed for testing Fyne GUI

--- drink.go
+++ drink.go
@@ -1,16 +1,51 @@
 package alcolator
 
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 
 // Drink is the basic struct for calculation
 type Drink struct {
-	VolumeML int     // Amount in milliliter
-	AlcPCT   float64 // Alcohol percentage
-	Price    float64 // Price, in unspecified currency
-	Name     string  // Name of the drink, for display purposes, not important really
-	Currency string  // For display purposes
+	VolumeML int     `json:"millilitres"`        // Amount in milliliter
+	AlcPCT   float64 `json:"percentage"`         // Alcohol percentage
+	Price    float64 `json:"price"`              // Price, in unspecified currency
+	Name     string  `json:"name"`               // Name of the drink, for display purposes, not important really
+	Currency string  `json:"currency,omitempty"` // For display purposes
 }
 
 type Drinks []Drink
+
+// Wrapper for strconv.Atoi
+func atoi(s string) int {
+	val, err := strconv.Atoi(s)
+	if nil != err {
+		return 0
+	}
+	return val
+}
+
+// Wrapper for strconv.ParseFloat
+func pfloat(s string) float64 {
+	val, err := strconv.ParseFloat(s, 32)
+	if nil != err {
+		return 0.0
+	}
+	return val
+}
+
+func DrinkFromStringFields(name, volume, percent, price, currency string) Drink {
+	return Drink{
+		Name:     name,
+		VolumeML: atoi(volume),
+		AlcPCT:   pfloat(percent),
+		Price:    pfloat(price),
+		Currency: currency,
+	}
+}
 
 // VolumeAsDL returns the drinks volume in DL (deciliter)
 func (d Drink) VolumeAsDL() float64 {
@@ -47,4 +82,47 @@ func (d Drink) PricePerDrinkML() float64 {
 // Add adds another drink to the slice
 func (ds *Drinks) Add(d Drink) {
 	*ds = append(*ds, d)
+}
+
+func (ds *Drinks) Load(r io.Reader) error {
+	jb, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(jb, ds)
+}
+
+func (ds *Drinks) LoadFile(fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = ds.Load(file)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ds Drinks) Save(w io.Writer) (int, error) {
+	jb, err := json.MarshalIndent(ds, "", "\t")
+	if err != nil {
+		return 0, err
+	}
+	jb = append(jb, '\n')
+	return w.Write(jb)
+}
+
+func (ds Drinks) SaveFile(fileName string) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = ds.Save(file)
+	if err != nil {
+		return err
+	}
+	return nil
 }
